dcb85dd658022bf98ab6d84f07e8edd0f6c7bf84
Crico-coder <haliullin2007@gmail.com>
2025-05-06 14:22:19 +0300

    Update main.c

--- main.c
+++ main.c
@@ -83,3 +83,13 @@ void delete_file(const char* filename, const char* target_file) {
     remove(filename);
     rename("temp.txt", filename);
 }
+
+int main() {
+    create_file("test.txt");
+    write_file("test.txt", "Hello, File System!");
+    char* content = read_file("test.txt");
+    printf("File content: %s\n", content);
+    list_files();
+    
+    return 0;
+}
