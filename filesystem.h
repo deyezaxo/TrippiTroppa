b87a4db7dc5ce08338065a990a85924434fc4014
Crico-coder <haliullin2007@gmail.com>
2025-05-06 14:25:41 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -4,7 +4,9 @@
 #include <stdio.h>
 
 FILE* fs_open_or_create(const char* filename);
-char* fs_view_file(const char* filename, const char* target_file);
-void fs_delete_file(const char* filename, const char* target_file);
+char* fs_read_file(const char* fs_content, const char* target_file);
+void fs_delete_file(const char* filename, const char* fs_content, const char* output_filename);
+void fs_create_file(const char* filename, const char* content, const char* fs_filename);
+void fs_modify_file(const char* filename, const char* new_content, const char* fs_content, const char* output_filename);
 
 #endif
