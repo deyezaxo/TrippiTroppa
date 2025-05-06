582aad710521a32ce808828fb305251ef8f65d0c
Crico-coder <haliullin2007@gmail.com>
2025-05-06 14:31:32 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,83 +3,21 @@
 #include <string.h>
 #include "filesystem.h"
 
-FILE* open_or_create_file(const char* filename) {
-    FILE* file = fopen(filename, "a+");
-    if (!file) {
-        perror("Ошибка открытия или создания файла");
-        exit(EXIT_FAILURE);
+int main() {
+    fs_create_new_file("test.txt", "Пример содержимого файла", "data.fs");
+    
+    char* content = fs_view_file("data.fs", "test.txt");
+    if (content) {
+        printf("Содержимое файла:\n%s\n", content);
+        free(content);
     }
-    return file;
-}
-
-char* view_file(const char* filename, const char* target_file) {
-    FILE* file = fopen(filename, "r");
-    if (!file) {
-        perror("Ошибка открытия файла для чтения");
-        return NULL;
-    }
-
-    char* buffer = NULL;
-    size_t size = 0;
-    size_t found = 0;
-    ssize_t read;
-
-    while ((read = getline(&buffer, &size, file)) != -1) {
-        if (strstr(buffer, target_file) != NULL) {
-            found = 1;
-            break;
-        }
-    }
-
-    if (found) {
-        char* content = malloc(1024);
-        size_t index = 0;
-        while ((read = getline(&buffer, &size, file)) != -1 && buffer[0] != '/') {
-            strcat(content + index, buffer);
-            index += read;
-        }
-        free(buffer);
-        fclose(file);
-        return content;
-    }
-
-    free(buffer);
+    
+    fs_modify_file("test.txt", "Обновленное содержимое", "data.fs", "data.fs.new");
+    
+    fs_delete_file("data.fs", "test.txt");
+    
+    FILE* file = fs_open_or_create("example.txt");
     fclose(file);
-    return NULL;
-}
-
-void delete_file(const char* filename, const char* target_file) {
-    FILE* file = fopen(filename, "r");
-    if (!file) {
-        perror("Ошибка открытия файла для чтения");
-        return;
-    }
-
-    FILE* temp_file = fopen("temp.txt", "w");
-    char* buffer = NULL;
-    size_t size = 0;
-    ssize_t read;
-
-    int skip = 0;
-    while ((read = getline(&buffer, &size, file)) != -1) {
-        if (strstr(buffer, target_file) != NULL) {
-            skip = 1;
-        }
-
-        if (skip) {
-            if (buffer[0] == '/') {
-                skip = 0;
-            }
-            continue;
-        }
-
-        fputs(buffer, temp_file);
-    }
-
-    fclose(file);
-    fclose(temp_file);
-    free(buffer);
-
-    remove(filename);
-    rename("temp.txt", filename);
+    
+    return 0;
 }
