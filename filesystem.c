0ab71645455dba464ca35dcace3b6599a648e94f
Crico-coder <haliullin2007@gmail.com>
2025-05-06 14:23:52 +0300

    Add files via upload

---
+++ filesystem.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "filesystem.h"
+
+FILE* fs_open_or_create(const char* filename) {
+    FILE* file = fopen(filename, "a+"); 
+    if (file == NULL) {
+        perror("Ошибка при открытии файла");
+        exit(EXIT_FAILURE);
+    }
+    return file;
+}
+
+char* fs_read_file(const char* fs_content, const char* target_file) {
+    char* content = strdup(fs_content);
+    char* line = strtok(content, "\n");
+    int found = 0;
+    char* result = NULL;
+
+    while (line != NULL) {
+        if (strcmp(line, target_file) == 0) {
+            found = 1;
+            line = strtok(NULL, "\n");
+            result = malloc(strlen(line) + 1);
+            strcpy(result, line);
+            break;
+        }
+        line = strtok(NULL, "\n");
+    }
+
+    free(content);
+    return found ? result : NULL;
+}
+
+void fs_delete_file(const char* filename, const char* fs_content, const char* output_filename) {
+    FILE* output = fopen(output_filename, "w");
+    char* content = strdup(fs_content);
+    char* line = strtok(content, "\n");
+    int skip = 0;
+
+    while (line != NULL) {
+        if (strcmp(line, filename) == 0) {
+            skip = 1;
+        } else if (line[0] == '/') {
+            skip = 0;
+        }
+
+        if (!skip) {
+            fprintf(output, "%s\n", line);
+        }
+        line = strtok(NULL, "\n");
+    }
+
+    free(content);
+    fclose(output);
+}
+
+void fs_create_file(const char* filename, const char* content, const char* fs_filename) {
+    FILE* fs_file = fopen(fs_filename, "a");
+    fprintf(fs_file, "%s\n%s\n/\n", filename, content);
+    fclose(fs_file);
+}
+
+void fs_modify_file(const char* filename, const char* new_content, const char* fs_content, const char* output_filename) {
+    FILE* output = fopen(output_filename, "w");
+    char* content = strdup(fs_content);
+    char* line = strtok(content, "\n");
+    int inside_target = 0;
+
+    while (line != NULL) {
+        if (strcmp(line, filename) == 0) {
+            inside_target = 1;
+            fprintf(output, "%s\n%s\n", line, new_content);
+            while (line != NULL && line[0] != '/') {
+                line = strtok(NULL, "\n");
+            }
+        } else {
+            fprintf(output, "%s\n", line);
+            line = strtok(NULL, "\n");
+        }
+    }
+    fclose(output);
+}
