6c463f5603591558105e0a149224f91818311add
Crico-coder <haliullin2007@gmail.com>
2025-05-06 14:18:54 +0300

    Update filesystem.h

--- filesystem.h
+++ filesystem.h
@@ -1,14 +1,12 @@
 #ifndef FILESYSTEM_H
 #define FILESYSTEM_H
 
-int create_file(const char* filename);
+#include <stdio.h>
 
-int delete_file(const char* filename);
-
-int write_file(const char* filename, const char* data);
-
-char* read_file(const char* filename);
-
-void list_files();
+FILE* fs_open_or_create(const char* filename);
+char* fs_read_file(const char* fs_content, const char* target_file);
+void fs_delete_file(const char* filename, const char* fs_content, const char* output_filename);
+void fs_create_file(const char* filename, const char* content, const char* fs_filename);
+void fs_modify_file(const char* filename, const char* new_content, const char* fs_content, const char* output_filename);
 
 #endif
